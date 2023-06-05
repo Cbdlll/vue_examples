d5d6ec28635908d4cb95dd73b430da79803c70c0
Cbdlll <1263982304@qq.com>
2023-06-05 16:12:14 +0800

    update

--- src/views/example03/FoodPageView.vue
+++ src/views/example03/FoodPageView.vue
@@ -1,4 +1,39 @@
 <template>
-  <RouterLink :to="`/example03/shops/12`" style="text-decoration: none"> shop </RouterLink>
-  <RouterView></RouterView>
+  <div>
+    <p>附近的美食</p>
+    <div v-for="(shop, index) of shopList" :key="index" style="min-width: 150px">
+      <router-link :to="`/example03/shops/${shop.id}`" class="router">
+        <div class="card">
+          <h4>{{ shop.name }}</h4>
+          评分：{{ shop.point }}
+        </div>
+      </router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useExp03Store } from './store'
+const store = useExp03Store()
+store.listShopsA()
+
+const shopList = computed(() => store.shopListS)
+</script>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.router {
+  color: #000;
+  text-decoration: none;
+}
+.card {
+  padding: 5px;
+}
+.card:hover {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+</style>
